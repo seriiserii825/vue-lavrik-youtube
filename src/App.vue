6c6eaf880f9a25ef128032ecadbac810776a82bd
serii burduja <seriiburduja@gmail.com>
2024-12-25 21:33:13 +0200

    feat: app vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts" setup>
+import {ref} from 'vue';
+
+const products = ref([
+  { id: 1, title: "Milk", cost: 100 },
+  { id: 2, title: "Salmon fish", cost: 200 },
+  { id: 3, title: "Vine", cost: 1000 },
+]);
+console.log(products.value, "products.value");
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container my-2">
+    <div class="row mb-3">
+      <div v-for="product in products" class="col">
+        <h2>{{ product.title }}</h2>
+        <div>{{ product.cost }}</div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
